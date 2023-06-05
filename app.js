bf36d578ca10aab640d8d35f35b939859160b9e7
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-06-04 21:37:43 -0500

    Se almacenaron los datos de los inputs title y description en variables

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 document.getElementById('formTask').addEventListener('submit' , saveTask);
 
 function saveTask(e) {
-    
 
+let title = document.getElementById('title').value;
+let description = document.getElementById('description').value;
 
     e.preventDefault()
 }
