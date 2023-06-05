b33771e02f3fb70e6972366587555eba4e3aea58
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-06-04 21:48:57 -0500

    Se creo una funcion para guardar las tareas usando localStorage

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ document.getElementById('formTask').addEventListener('submit' , saveTask);
 
 function saveTask(e) {
 
-let title = document.getElementById('title').value;
-let description = document.getElementById('description').value;
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
 
-    e.preventDefault()
+    const task = {
+        title,
+        description
+    };
+
+    if (localStorage.getItem('tasks') === null) {
+
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    } else {
+
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    }
+
+    e.preventDefault();
 }
+
